ffad336efccf2227992472790fa962532393c339
Julia Bode <julia.bode@lulisaur.us>
2012-08-16 14:59:12 +0200

    Redesign homepage look.

--- config/initializers/hooks.php
+++ config/initializers/hooks.php
@@ -15,3 +15,43 @@ function change_default_title( $title ){
 }
 
 add_filter( 'enter_title_here', 'change_default_title' );
+
+/*
+ * The Theme setting page
+ */
+
+function chiharu_add_menu() {
+  add_menu_page('Extra Theme Options', 'Extra Options', 'manage_options', 'extra_theme_options', 'chiharu_settings_page_fn');
+}
+
+function chiharu_settings_page_fn() { ?>
+
+  <div class="wrap">
+    <h2>Extra Theme Options</h2>
+
+    <form action="options.php" method="post">
+
+      <?php settings_fields( 'chiharu-theme-group' ); ?>
+
+      <table class="form-table">
+          <tr valign="top">
+          <th scope="row">Homepage Image Url</th>
+          <td><input style="width:600px;" type="text" name="homepage_image_url" value="<?php echo get_option('homepage_image_url'); ?>" /></td>
+          </tr>
+      </table>
+
+      <p class="submit">
+        <input name="Submit" type="submit" class="button-primary" value='Save Changes' />
+      </p>
+
+    </form>
+  </div>
+
+<?php }
+
+function chiharu_register_settings() {
+  register_setting('chiharu-theme-group', 'homepage_image_url' );
+}
+
+add_action( 'admin_menu', 'chiharu_add_menu' );
+add_action( 'admin_init', 'chiharu_register_settings' );
